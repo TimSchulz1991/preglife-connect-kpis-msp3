2b4eab12178055df2a870d1986a17070ee6a04c0
Tim Schulz <tim@getbej.com>
2022-01-31 08:40:53 +0000

    Get a date from the user and validate that it is an accepted date

--- run.py
+++ run.py
@@ -12,19 +12,53 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("connect_kpis")
+WORKSHEET = SHEET.worksheet("kpis")
 
 #kpis = SHEET.worksheet("kpis")
 #data = kpis.get_all_values()
 #print(data)
+
+print("Welcome to this Python program to save the most recent Preglife Connect KPIs\nand to analyse the current trends for you!\n")
 
 def get_date():
     """
     This function is used to get the date from the user for which the KPIs should be entered into the sheet. 
     """
     while True:
-        print("For which date would you like to enter the KPIs or Preglife Connect?")
+        print("For which date would you like to enter the KPIs of the Preglife Connect app?")
         print("Please enter the date in the following format: DD/MM/YYYY, e.g. 22/02/2022\n")
         date_input = input("Enter the date here:\n")
-        print(date_input)
+        
+        if validate_date(date_input):
+            print("Thank you for entering a valid date.\n")
+            break
+        
+    return date_input
 
-get_date()
+def validate_date(date):
+    """
+    This function checks if the given date exists in the sheet and can be filled with values.
+    """
+    if WORKSHEET.find(date) is not None:
+        return True
+    else: 
+        print("That was not a valid date, please try again.\n")
+        return False
+
+def get_kpis():
+    date = get_date()
+    kpi_list = []
+    promts = [f"Please enter the number of APP OPENS for {date}:\n","Please enter the number of SCREEN VIEWS:\n",
+    "Please enter the number of AD VIEWS:\n","Please enter the number of CREATED THREADS:\n",
+    "Please enter the number of SWIPES:\n"]
+    for i in range(len(promts)):
+        kpi_input = input(promts[i])
+        kpi_list.append(kpi_input)
+    print(kpi_list)
+
+
+def main():
+    get_date()
+    #get_kpis()
+
+main()
