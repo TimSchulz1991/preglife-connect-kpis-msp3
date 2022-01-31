329f8183c6703d2a368fb2462e0ba5eb68746bb3
Tim Schulz <tim@getbej.com>
2022-01-31 13:16:34 +0000

    Fix all the Python related styling errors

--- run.py
+++ run.py
@@ -14,43 +14,57 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("connect_kpis")
 WORKSHEET = SHEET.worksheet("kpis")
 
-#kpis = SHEET.worksheet("kpis")
-#data = kpis.get_all_values()
-#print(data)
+print("""Welcome to this Python program to save the most recent
+Preglife Connect KPIs and to analyse the current trends for you!\n""")
 
-print("Welcome to this Python program to save the most recent Preglife Connect KPIs\nand to analyse the current trends for you!\n")
 
 def get_date():
     """
-    This function is used to get the date from the user for which the KPIs should be entered into the sheet. 
+    This function is used to get the date from the user
+    for which the KPIs should be entered into the sheet.
     """
     while True:
-        print("For which date would you like to enter the KPIs of the Preglife Connect app?")
-        print("Please enter the date in the following format: DD/MM/YYYY, e.g. 22/02/2022\n")
+        print("""For which date would you like to enter
+        the KPIs of the Preglife Connect app?""")
+        print("""Please enter the date in the following format:
+        DD/MM/YYYY, e.g. 22/02/2022\n""")
+
         date_input = input("Enter the date here:\n")
-        
+
         if validate_date(date_input):
             print("Thank you for entering a valid date.\n")
             break
-        
     return date_input
+
 
 def validate_date(date):
     """
-    This function checks if the given date exists in the sheet and can be filled with values.
+    This function checks if the given date exists
+    in the sheet and can be filled with values.
     """
     if WORKSHEET.find(date) is not None:
         return True
-    else: 
+    else:
         print("That was not a valid date, please try again.\n")
         return False
 
+
 def get_kpis():
+    """
+    This function gets all the KPIs from the user and saves them in a list.
+    """
     date = get_date()
     kpi_list = []
-    promts = [f"Please enter the number of APP OPENS for {date}:\n","Please enter the number of SCREEN VIEWS:\n",
-    "Please enter the number of AD VIEWS:\n","Please enter the number of CREATED THREADS:\n",
-    "Please enter the number of SWIPES:\n"]
+    promts = [f"""Please enter the number of APP OPENS
+              for the chosen date ({date}):\n""",
+              f"""Please enter the number of SCREEN VIEWS
+              for the chosen date ({date}):\n""",
+              f"""Please enter the number of AD VIEWS
+              for the chosen date ({date}):\n""",
+              f"""Please enter the number of CREATED THREADS
+               for the chosen date ({date}):\n""",
+              f"""Please enter the number of SWIPES
+              for the chosen date ({date}):\n"""]
     for i in range(len(promts)):
         kpi_input = input(promts[i])
         kpi_list.append(kpi_input)
@@ -58,7 +72,7 @@ def get_kpis():
 
 
 def main():
-    get_date()
-    #get_kpis()
+    get_kpis()
+
 
 main()
